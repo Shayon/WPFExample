37a2fc32c5941eb6cf08b14fae03ba632b1a3f8e
Shayon Javadizadeh <shyjavadi@gmail.com>
2014-10-17 15:18:03 -0700

    recommitting

---
+++ WPFExample/Views/Controls/AddAsignmentControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFExample.ViewModels;
+
+namespace WPFExample.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for AddAsignmentControl.xaml
+    /// </summary>
+    public partial class AddAsignmentControl : UserControl
+    {
+        public AddAsignmentControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AssignmentFormVM form = (AssignmentFormVM) (((Button) sender).DataContext);
+            AssignmentVM assign = new AssignmentVM(form.FormNameField, form.FormTotalField, form.FormGradeField);
+            ((StudentVM) this.DataContext).AssignmentList.Add(assign);
+            ((StudentVM) this.DataContext).EvaluateGrade();
+        }
+    }
+}
