c5f874fa9c24e72c17f647312e4110e1c2c3dfaa
Shayon Javadizadeh <shyjavadi@gmail.com>
2014-10-17 14:37:56 -0700

    Beginning stages of abstraction

---
+++ WPFExample/Views/Converters/LetterGradeConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPFExample.Views.Converters
+{
+    class LetterGradeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            if(value is double)
+            {
+                double percentage = (double) value;
+                string gradeString="";
+                if (percentage >= .90)
+                {
+                    gradeString = "A";
+                }
+                else if (percentage >= .80)
+                {
+                    gradeString = "B";
+                }
+                else if (percentage >= .70)
+                {
+                    gradeString = "C";
+                }
+                else if (percentage >= .60)
+                {
+                    gradeString = "D";
+                }
+                else if (percentage < .60)
+                {
+                    gradeString = "F";
+                }
+                if (gradeString != "F")
+                {
+                    if (percentage % .1 >= .07 || percentage >= 1)
+                    {
+                        gradeString = gradeString + "+";
+                    }
+                    else if (percentage % .1 <= .03)
+                    {
+                        gradeString = gradeString + "-";
+                    }
+                }
+                return gradeString;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPFExample/Views/Validations/AssignmentTotalValidation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPFExample.Views.Validations
+{
+    class AssignmentTotalValidation : ValidationRule
+    {
+        private int _grade;
+
+        public int Grade
+        {
+            get { return _grade; }
+
+            set
+            {
+                _grade = value;
+            }
+        }
+
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            int total=-1;
+            try
+            {
+                if (((string) value).Length > 0)
+                    total = Int32.Parse((String) value);
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, "Illegal characters or " + e.Message);
+            }
+            if(total <= 0)
+            {
+                return new ValidationResult(false, "Please enter a total greater than 0.");
+            }
+            else
+            {
+                return new ValidationResult(true, null);
+            }
+        }
+
+    }
+}
